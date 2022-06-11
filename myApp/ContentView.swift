295382adbe35c5148e5643b7ebf258c34e191dcc
SoHyeon Kim <happythgus89@gmail.com>
2022-06-11 16:44:10 +0900

    Initial Commit

---
+++ myApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myApp
+//
+//  Created by 김소현 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by 김소현 on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
